1de5d345b902fe859cd304528d886448225fc21f
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2023-12-27 21:44:53 -0800

    Fix reset button

--- main.js
+++ main.js
@@ -41,6 +41,5 @@ function changeColor(choice) {
 function resetBoard() {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
-    squares.forEach(div => div.remove()); 
-    populateBoard(16);
+    squares.forEach(div => div.style.backgroundColor = 'white');
 }
