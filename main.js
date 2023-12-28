318199e163b1552d8abbcba5e5ac81f9bbc90934
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2023-12-27 22:08:56 -0800

    Add color toggle message

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let color = "black";
+let click = true;
 
 function populateBoard(size) {
     let board = document.querySelector(".board");
@@ -27,11 +28,13 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-        if (color === "random") {
+    if (click) {
+        if (color === "random") {   
             this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
         } else {
             this.style.backgroundColor = color;
         }
+    }
 }
 
 function changeColor(choice) {
@@ -43,3 +46,12 @@ function resetBoard() {
     let squares = board.querySelectorAll("div");
     squares.forEach(div => div.style.backgroundColor = 'white');
 }
+
+document.querySelector('body').addEventListener('click', () => {
+    click = !click;
+    if (click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring";
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"; 
+    }
+});
